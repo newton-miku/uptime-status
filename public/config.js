af221af957110c104d2ffce2dda50b6bd129501e
newton_miku <1316561519@qq.com>
2022-08-16 11:24:59 +0800

    Update config.js

--- public/config.js
+++ public/config.js
@@ -6,10 +6,11 @@ window.Config = {
   // UptimeRobot Api Keys
   // 支持 Monitor-Specific 和 Read-Only
   ApiKeys: [
-    'm784488775-dd1ad84b209c05f8e185c33e',
-    'm784490063-7b5da437e7f1e0d67613714d',
-    'm784497419-de55aa09902ccb3ab22d548a',
-    'm784496436-71a4bf7b1e3bdf7756be131b',
+    'm792477080-393f5d43efd8da8aba28b25b',
+    'm792477097-d8834077b41c60cc7a6ae58c',
+    'm792477266-39a08d31a9e51e8e70445e34',
+    'm792477075-a9f9dc72a040931ecb0b4d97',
+    'm792477271-14d2afb33a4dbed68b36db6a',
   ],
 
   // 日志天数
@@ -30,7 +31,7 @@ window.Config = {
     },
     {
       text: 'Blog',
-      url: 'https://abo.xyz/'
+      url: 'https://www.ddxnb.tk/wordpress'
     },
   ],
 };
