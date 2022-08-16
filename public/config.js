035f59f58a21a69fb2e9a247311854e38cc45ad7
newton_miku <1316561519@qq.com>
2022-08-16 11:41:26 +0800

    Update config.js

--- public/config.js
+++ public/config.js
@@ -7,10 +7,10 @@ window.Config = {
   // 支持 Monitor-Specific 和 Read-Only
   ApiKeys: [
     'm792477080-393f5d43efd8da8aba28b25b',
-    'm792477097-d8834077b41c60cc7a6ae58c',
     'm792477266-39a08d31a9e51e8e70445e34',
     'm792477075-a9f9dc72a040931ecb0b4d97',
     'm792477271-14d2afb33a4dbed68b36db6a',
+    'm792477097-d8834077b41c60cc7a6ae58c',
   ],
 
   // 日志天数
